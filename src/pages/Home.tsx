b1cef971351500ed55a49693f7cf70355db97ffd
Tam-BobManuel <bglorytb@gmail.com>
2024-02-14 13:24:18 +0100

    remove chicken background img from home route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react";
 import AboutUs from "../components/AboutUs";
 import Slideshow from "../components/SlideShow";
 import data from "../data/homeData.json";
-import homeAboutImg from "../assets/images/background/photo-1627462656780-964d9535a50b.avif";
 
 function Home() {
   const {
@@ -17,7 +16,7 @@ function Home() {
   return (
     <div style={{ textAlign: "center" }}>
       <Slideshow images={images} textOverlays={textOverlays} />
-      <Box bgImage={homeAboutImg} bgSize="cover" bgPosition="center">
+      <Box bgSize="cover" bgPosition="center">
         <div style={{ margin: "0 auto", maxWidth: "100%" }}>
           {aboutUsHeadings.map((heading, index) => (
             <AboutUs
