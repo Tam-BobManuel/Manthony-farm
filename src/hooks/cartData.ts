90c87d2ab86a20c2a6eab8b4eea5650ee486a563
Tam-BobManuel <bglorytb@gmail.com>
2024-05-06 18:42:53 +0100

    update cartIcon and cartData

--- src/hooks/cartData.ts
+++ src/hooks/cartData.ts
@@ -1,7 +1,10 @@
-export const cartItems: number[] = JSON.parse(sessionStorage.getItem("cartItems") || "[]");
+import { useState } from "react";
+
+export const [cartItems, setCartItems] = useState<number[]>(JSON.parse(sessionStorage.getItem("cartItems") || "[]"));
 
 // Function to update cartItems array and store it in sessionStorage or localStorage
 export const updateCartItems = (newCartItems: number[]) => {
+  setCartItems(newCartItems);
   sessionStorage.setItem("cartItems", JSON.stringify(newCartItems));
   // localStorage.setItem("cartItems", JSON.stringify(newCartItems));
 };
